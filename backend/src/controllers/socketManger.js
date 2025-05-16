ec4970364ce473de84657045e2fef9274f07862a
aryaW <aryawankhade25@gmail.com>
2025-05-17 02:27:26 +0530

    Update socketManger.js

--- backend/src/controllers/socketManger.js
+++ backend/src/controllers/socketManger.js
@@ -1,8 +1,24 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
-
+  let connections = {}
+  let messages = {}
+  let timeOnline = {}
  export const connectToSocket = (server)=>{
    const io = new Server(server);
+
+   io.on("conection" , (Socket)=>{
+
+    Socket.on("join-call",(path)=>{
+
+    })
+    socket.on("signal",(toId ,messages)=>{
+      io.to(toId).emit("signal",socket.id,message);
+
+    })
+
+   })
+
+
 
    return io;
 }
