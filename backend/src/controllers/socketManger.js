401726ce9dbe636c57fc4fc519e8a81cad5b4069
aryaW <aryawankhade25@gmail.com>
2025-05-17 23:45:14 +0530

    Update socketManger.js

--- backend/src/controllers/socketManger.js
+++ backend/src/controllers/socketManger.js
@@ -47,7 +47,7 @@ export const connectToSocket = (server) => {
         return [room,isFound];
       },['',false]);
 
-      if(found == true){
+      if(found === true){
         if(messages[matchingRoom] === undefined){
           messages[matchingRoom] = []
         }
@@ -67,7 +67,24 @@ export const connectToSocket = (server) => {
       var key 
 
       for(const[k,v] of JSON.parse(JSON.stringify(Object.entries(connections)))){
-                 
+              
+        for(let a=0; a<v.length; ++a){
+          if(v[a] === socket.id){
+            key = k
+
+            for(let a=0; a<connections[key].length; ++a){
+              io.to(connections[key][a].emit('user-left',socket.id))
+            }
+
+            var index = connections[key].indexOf(socket.id)
+
+            connections[key].splice(index,1)
+
+            if(connections[key].length === 0){
+              delete connections[key]
+            }
+          }
+        }
       }
     })
   });
