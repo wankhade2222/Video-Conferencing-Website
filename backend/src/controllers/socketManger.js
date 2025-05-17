a48766549c9f73393a38f11ecff1633038393c77
aryaW <aryawankhade25@gmail.com>
2025-05-17 17:15:14 +0530

    Update socketManger.js

--- backend/src/controllers/socketManger.js
+++ backend/src/controllers/socketManger.js
@@ -7,12 +7,27 @@ export const connectToSocket = (server) => {
   const io = new Server(server);
 
   io.on("conection", (Socket) => {
+
+    console.log("SOMETHING CONNECTED");
+
     Socket.on("join-call", (path) => {
 
       if(connections[path] === undefined){
         connections[path] = []
       }
       connections[path].push(socket.id)
+
+      timeOnline[socket.id] = new Date();
+
+      for(let a=0; a<connections[path].length; a++){
+        io.to(connections[path][a]).emit("user-joined",socket.id,connections[path])
+      }
+
+      if(messages[path] != undefined){
+        for(let a=0; a<messages[path].length; ++a  ){
+          io.to(socket.id).emit("chat-message")
+        }
+      }
     });
 
     socket.on("signal", (toId, messages) => {
