df260cc7596148d4158d20df3be6730e2d5e7ca7
aryaW <aryawankhade25@gmail.com>
2025-05-19 21:59:25 +0530

    commit

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
-
+import React from "react";
+import "../App.css";
 function LandingPage() {
-  return <div>LandingPage</div>;
+  return <div className="landingPageContainer">
+    <nav>
+      <div className="navHeader">
+        <h2>video conferencing website</h2>
+      </div>
+      <div className="navList"></div>
+    </nav>
+  </div>;
 }
 
 export default LandingPage;
