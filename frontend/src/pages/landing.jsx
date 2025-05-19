8a4f731547054c3e24b43d1cf32c2345fd3e4fd2
aryaW <aryawankhade25@gmail.com>
2025-05-19 23:22:07 +0530

    commit

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../App.css";
+import { Link} from 'react-router-dom'
+
 function LandingPage() {
   return (
     <div className="landingPageContainer">
@@ -7,7 +9,7 @@ function LandingPage() {
         <div className="navHeader">
           <h2>video conferencing website</h2>
         </div>
-        <div className="navList">
+        <div className="navlist">
           <p>Join as Guest</p>
           <p>Register</p>
           <div role="button">
@@ -24,11 +26,11 @@ function LandingPage() {
           </h1>
           <p>Cover a distance by video Call </p>
           <div role="button">
-            <link to={"/auth"}>Get Started</link>
+            <Link to={"/auth"}>Get Started</Link>
           </div>
         </div>
         <div>
-          <img src="" alt="" />
+          <img src="mobile.png" alt="" />
         </div>
       </div>
     </div>
