cb65578a2872b95705d7b18594e350b299386094
aryaW <aryawankhade25@gmail.com>
2025-05-19 22:50:45 +0530

    Update landing.jsx

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,14 +1,38 @@
 import React from "react";
 import "../App.css";
 function LandingPage() {
-  return <div className="landingPageContainer">
-    <nav>
-      <div className="navHeader">
-        <h2>video conferencing website</h2>
+  return (
+    <div className="landingPageContainer">
+      <nav>
+        <div className="navHeader">
+          <h2>video conferencing website</h2>
+        </div>
+        <div className="navList">
+          <p>Join as Guest</p>
+          <p>Register</p>
+          <div role="button">
+            <p>Login</p>
+          </div>
+        </div>
+      </nav>
+
+      <div className="landingMainContainer">
+        <div>
+          <h1>
+            <span style={{ color: "#FF9839" }}>Connect</span> with your loved
+            Ones
+          </h1>
+          <p>Cover a distance by video Call </p>
+          <div role="button">
+            <link to={"/auth"}>Get Started</link>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
       </div>
-      <div className="navList"></div>
-    </nav>
-  </div>;
+    </div>
+  );
 }
 
 export default LandingPage;
